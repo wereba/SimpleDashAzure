d99e6d10f5e8071ed24cd6f50768e86d05d23d85
wereba <samuelwereba@gmail.com>
2023-02-10 06:55:47 +0300

    Add files via upload

---
+++ SimpleDashAzure.py
@@ -0,0 +1,29 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.express as px
+from azure.storage.blob import ContainerClient, BlobClient
+import io
+
+container_name = "gsr"
+blob_name = "Twatter.csv"
+account_url = "https://sam123.blob.core.windows.net"
+
+container_client = ContainerClient(account_url=account_url, container_name=container_name)
+blob_client = BlobClient(account_url=account_url, container_name=container_name, blob_name=blob_name)
+
+blob_data = blob_client.download_blob().readall().decode('utf-8')
+df = pd.read_csv(io.StringIO(blob_data))
+dash_app = dash.Dash(__name__)
+app = dash_app.server
+
+
+dash_app.layout = html.Div([
+    dcc.Graph(
+        id='graph',
+        figure= px.scatter(df, x='Date', y='Open'))
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
